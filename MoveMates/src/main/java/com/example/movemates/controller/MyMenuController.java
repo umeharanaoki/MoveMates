58591ea9234e001511049ef51bcb52d95ff42296
N.umehara <n6773938@gmail.com>
2024-04-01 03:33:52 +0900

    Temporary save before changing form submission method

--- MoveMates/src/main/java/com/example/movemates/controller/MyMenuController.java
+++ MoveMates/src/main/java/com/example/movemates/controller/MyMenuController.java
@@ -1,7 +1,6 @@
 package com.example.movemates.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.movemates.entity.Exercise;
 import com.example.movemates.entity.MyMenu;
-import com.example.movemates.entity.MyMenuExercise;
 import com.example.movemates.entity.User;
 import com.example.movemates.form.ExerciseLogForm;
 import com.example.movemates.form.MyMenuEditForm;
@@ -57,103 +56,35 @@ public class MyMenuController {
 	
 	// マイメニュー編集ページ
 	@GetMapping("/{mymenu_id}/edit")
-	public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable(name = "mymenu_id") Integer mymenuId, Model model) {
-		User user = userDetailsImpl.getUser();
-		List<Exercise> exerciseList = exerciseRepository.findAll();
-		MyMenu myMenu = myMenuRepository.getReferenceById(mymenuId);
-		
-		// ビュー側でjavascriptによるリストの更新をやりやすくするためにInteger型のListを準備
-		// フォームに渡すためにオブジェクトのリストをIDのリストに変更
-		List<Integer> myMenuExerciseIds = myMenu.getMyMenuExercises().stream()
-				.map(MyMenuExercise::getExercise)
-				.map(Exercise::getId)
-				.collect(Collectors.toList());
-		
-		// 全てのエクササイズのIDを取得
-	    List<Integer> allExerciseIds = exerciseRepository.findAll().stream()
-	            .map(Exercise::getId)
-	            .collect(Collectors.toList());
-	    // 選択肢として表示するエクササイズのIDリストを作成
-	    // すでに登録されているエクササイズを除外する
-	    List<Integer> selectableExerciseIds = allExerciseIds.stream()
-	            .filter(exerciseId -> !myMenuExerciseIds.contains(exerciseId))
-	            .collect(Collectors.toList());
-		
-	    // ビューに渡すためのフォームのインスタンスを作成
-	 	MyMenuEditForm myMenuEditForm = new MyMenuEditForm(mymenuId, myMenu.getName(), myMenuExerciseIds);
-	    
-		model.addAttribute("user", user);
-		model.addAttribute("exerciseList", exerciseList);
-		model.addAttribute("myMenu", myMenu);
-		model.addAttribute("myMenuEditForm", myMenuEditForm);
-		model.addAttribute("myMenuExerciseIds", myMenuExerciseIds);
-		model.addAttribute("selectableExercises", selectableExerciseIds);
-		
-		return "mymenu/edit";
-	}
-
-	
-	
-	@GetMapping("/{mymenu_id}/vue-test")
 	public String test(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable(name = "mymenu_id") Integer mymenuId, Model model) {
 		User user = userDetailsImpl.getUser();
 		MyMenu myMenu = myMenuRepository.getReferenceById(mymenuId);
-//		// すべてのexerciseのリスト
-//		List<Exercise> allExercises = exerciseRepository.findAll();
-//		// 全てのエクササイズのIDを取得
-//		List<Integer> allExerciseIds = exerciseRepository.findAll().stream()
-//				.map(Exercise::getId)
-//				.collect(Collectors.toList());
-//		// mymenuに含まれるexerciseのリスト
-//		List<MyMenuExercise> myMenuExercises = myMenu.getMyMenuExercises();
-		// mymenuに含まれるexerciseのIDリスト
-		List<Integer> myMenuExerciseIds = myMenu.getMyMenuExercises().stream()
-				.map(MyMenuExercise::getExercise)
-				.map(Exercise::getId)
-				.collect(Collectors.toList());
-		
-//	    // 選択肢として表示するエクササイズのIDリストを作成
-//		// マイメニューに含まれないエクササイズのみを抽出
-//		List<Exercise> selectableExercises = allExercises.stream()
-//				.filter(exercise -> !myMenuExerciseIds.contains(exercise.getId()))
-//				.collect(Collectors.toList());
-//	    // すでに登録されているエクササイズを除外する
-//	    List<Integer> candidateExerciseIds = allExerciseIds.stream()
-//	            .filter(exerciseId -> !myMenuExerciseIds.contains(exerciseId))
-//	            .collect(Collectors.toList());
-		
-//		// MyMenuオブジェクトのID情報をJSON形式で送る
-//	    ObjectMapper objectMapper = new ObjectMapper();
-//	    try {
-//	        String myMenuIdJson = objectMapper.writeValueAsString(mymenuId);
-//	        model.addAttribute("myMenuIdJson", myMenuIdJson);
-//	    } catch (JsonProcessingException e) {
-//	        // JSON形式への変換中にエラーが発生した場合の処理
-//	        e.printStackTrace();
-//	    }
 		
 	    // ビューに渡すためのフォームのインスタンスを作成
-	 	MyMenuEditForm myMenuEditForm = new MyMenuEditForm(mymenuId, myMenu.getName(), myMenuExerciseIds);
+		// MyMenuに属するExerciseの情報はMyMenuのIDを用いてMyMenuEditControllerで取得するのでここではnull
+	 	MyMenuEditForm myMenuEditForm = new MyMenuEditForm(mymenuId, myMenu.getName(), null);
 	    
 		model.addAttribute("user", user);
 		model.addAttribute("myMenu", myMenu);
-		model.addAttribute("menuId", myMenu.getId());
-//		model.addAttribute("exerciseList", allExercises);
 		model.addAttribute("myMenuEditForm", myMenuEditForm);
-//		model.addAttribute("myMenuExerciseIds", myMenuExerciseIds);
-//		model.addAttribute("candidateExerciseIds", candidateExerciseIds);
-		return "mymenu/vue-test";
+		return "mymenu/edit";
 	}
 	
-	
-	
-	
-	
 	// MyMenuEditFormの処理
-//	@PostMapping("/update")
-//	public String update(Model model) {
-//		
-//	}
+	@PostMapping("/update")
+	public String update(@RequestBody MyMenuEditForm myMenuEditForm, RedirectAttributes redirectAttributes) {
+//		if(bindingResult.hasErrors()) {
+//			return "mymenu/edit";
+//		}
+		
+		System.out.println(myMenuEditForm.getMyMenuId());
+		System.out.println(myMenuEditForm.getMyMenuName());
+		System.out.println(myMenuEditForm.getMyMenuExercises());
+		
+		redirectAttributes.addFlashAttribute("successMessage", myMenuEditForm.getMyMenuName() + "を更新しました。");
+		
+		return "redirect:/mymenu";
+	}
 	
 	// 運動開始
 	@GetMapping("/{mymenu_id}/start")
--- MoveMates/src/main/java/com/example/movemates/form/MyMenuEditForm.java
+++ MoveMates/src/main/java/com/example/movemates/form/MyMenuEditForm.java
@@ -2,6 +2,8 @@ package com.example.movemates.form;
 
 import java.util.List;
 
+import com.example.movemates.entity.Exercise;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -16,14 +18,5 @@ public class MyMenuEditForm {
 	@NotBlank(message = "メニュー名を入力してください。")
 	private String myMenuName;
 	
-	// Entityのリストにすると必要ないフィールドが混ざるため修正
-	// メニューに属するエクササイズがなくても問題ない（初期状態）ので@NotNullも不要
-//	@NotNull
-//	private List<MyMenuExercise> myMenuExercises;
-	
-	private List<Integer> myMenuExerciseIds;
-	
-	// 順序はListから取得できるのでフィールドに定義する必要はない
-//	@NotNull
-//	private List<Integer> exerciseOrder;
+	private List<Exercise> myMenuExercises;
 }
